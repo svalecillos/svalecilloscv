30c2f97a231eeb3e35f31cf55e36f3587d1c1391
Saul Valecillos <svalecillos.93@gmail.com>
2019-11-10 17:42:49 -0400

    subiendo aplicacion

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+    
     $('.scrollspy').scrollSpy({
       scrollOffset: 60 //Offset for centering element when scrolled to.
     });
+
     //Metodo scroll para poner el nav transparente dependiendo donde este el scroll
     $(window).scroll(function(){
         if($(window).scrollTop()>580){
@@ -14,20 +16,24 @@ $(document).ready(function(){
       });
 
     $('.sidenav').sidenav();// -> Iniciamos el boton del menu para moviles
-
 });
 
+//Selecion de opciones del nav
 $(window).on('scroll', function () {
-   //Selecion de opciones del nav
-   let posicion = $(window).scrollTop();
-   let position2 = posicion+60;
-   console.log("posicion: "+position2);
-   console.log("about "+$("#about").offset().top);
-   console.log("contact"+$("#contact").offset().top);
 
+  let posicion = $(window).scrollTop();
+  let position2 = posicion+60;
+   
    if( position2 > $("#home").offset().top) { highlightLink('home'); }
    if( position2 > $("#about").offset().top) {  highlightLink('about'); }
-   if( position2 > $("#skill").offset().top) {  highlightLink('skill'); }
+   if( position2 > $("#skill").offset().top) {  
+        highlightLink('skill'); 
+        $('.skillbar').each(function(){
+          $(this).find('.skillbar-bar').animate({
+              width:jQuery(this).attr('data-percent')
+          },1000);
+        });
+      }
    if( position2 > $("#works").offset().top) {  highlightLink('works'); }
    if( position2 > $("#contact").offset().top || 
        posicion + $(window).height() === $(document).height()) {  
